f44ea29d7a6bc0436099d1eb6bfb8d3deb6610fe
Eyob Tamiru <eyobtamiru111@gmail.com>
2025-09-25 09:17:39 +0300

    Updated multiple files: navbar, about page, and styles

---
+++ js/script phy.js
@@ -0,0 +1,159 @@
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+
+
+const quizArray = [
+  {
+    id: "0",
+    question: "Work done will be maximum if the angle between the force F and displacement d is?",
+    options: ["45", "90", "0", "180"],
+    correct: "0",
+  },
+  {id: "1",
+    question: "A field in which the work done in a moving a body along closed path is zero is called?",
+    options: ["electric field", "conservative field", "electromagnetic field", "gravitational field"],
+    correct: "conservative field",
+},
+   {
+    id: "2",
+    question: "When a force is parallel to the direction of motion of the body, then work done on the body is?",
+    options: ["zero", "minimum", "infinity", "maximum"],
+    correct: "maximum",
+  },
+  {
+    id: "3",
+    question: "Which of the following types of force can do no work on the particle on which it acts?",
+    options: ["elastic force", "centripetal force", "gravitational force", "frictional force"],
+    correct: "centripetal force",
+  },
+  {
+    id: "4",
+    question: "If a body a mass of 2 kg is raised vertically through 2m, then the work done will be?",
+    options: ["38.2 J", "39.2 J", "392.1 J", "3.92 J"],
+    correct: "39.2 J",
+  },
+];
+
+restart.addEventListener("click", () => {
+  initial();
+  displayContainer.classList.remove("hide");
+  scoreContainer.classList.add("hide");
+});
+
+nextBtn.addEventListener(
+  "click",
+  (displayNext = () => {
+    questionCount += 1;
+    if (questionCount == quizArray.length) {
+      displayContainer.classList.add("hide");
+      scoreContainer.classList.remove("hide");
+      userScore.innerHTML =
+        "Your score is " + scoreCount + " out of " + questionCount;
+    } else {
+      countOfQuestion.innerHTML =
+        questionCount + 1 + " of " + quizArray.length + " Question";
+      quizDisplay(questionCount);
+      count = 11;
+      clearInterval(countdown);
+      timerDisplay();
+    }
+  })
+);
+
+const timerDisplay = () => {
+  countdown = setInterval(() => {
+    count--;
+    timeLeft.innerHTML = `${count}s`;
+    if (count == 0) {
+      clearInterval(countdown);
+      displayNext();
+    }
+  }, 1000);
+};
+
+const quizDisplay = (questionCount) => {
+  let quizCards = document.querySelectorAll(".container-mid");
+  quizCards.forEach((card) => {
+    card.classList.add("hide");
+  });
+  quizCards[questionCount].classList.remove("hide");
+};
+
+function quizCreator() {
+  quizArray.sort(() => Math.random() - 0.5);
+  for (let i of quizArray) {
+    i.options.sort(() => Math.random() - 0.5);
+    let div = document.createElement("div");
+    div.classList.add("container-mid", "hide");
+    countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+    let question_DIV = document.createElement("p");
+    question_DIV.classList.add("question");
+    question_DIV.innerHTML = i.question;
+    div.appendChild(question_DIV);
+    div.innerHTML += `
+    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+      <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+    `;
+    quizContainer.appendChild(div);
+  }
+}
+
+function checker(userOption) {
+  let userSolution = userOption.innerText;
+  let question =
+    document.getElementsByClassName("container-mid")[questionCount];
+  let options = question.querySelectorAll(".option-div");
+
+  if (userSolution === quizArray[questionCount].correct) {
+    userOption.classList.add("correct");
+    scoreCount++;
+  } else {
+    userOption.classList.add("incorrect");
+    options.forEach((element) => {
+      if (element.innerText == quizArray[questionCount].correct) {
+        element.classList.add("correct");
+      }
+    });
+  }
+
+  clearInterval(countdown);
+  options.forEach((element) => {
+    element.disabled = true;
+  });
+}
+
+function initial() {
+  quizContainer.innerHTML = "";
+  questionCount = 0;
+  scoreCount = 0;
+  count = 11;
+  clearInterval(countdown);
+  timerDisplay();
+  quizCreator();
+  quizDisplay(questionCount);
+}
+
+startButton.addEventListener("click", () => {
+  startScreen.classList.add("hide");
+  displayContainer.classList.remove("hide");
+  initial();
+});
+
+window.onload = () => {
+  startScreen.classList.remove("hide");
+  displayContainer.classList.add("hide");
+};
